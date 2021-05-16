d72a0321770121725f3146205ed63b8b88864000
Andrii Aheiev <a.ageyev@gmail.com>
2021-05-16 17:04:46 +0200

    version controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  def version
+    version_file_path   = "#{Rails.root}/VERSION"
+    application_version = File.read(version_file_path).chomp if File.file?(version_file_path)
+    assembly_version    = ENV['ASSEMBLY_VERSION'] || 'undefined'
+    @version = {
+      application_version: application_version,
+      assembly_version:    assembly_version
+    }
+    respond_to do |format|
+      format.json { render json: @version }
+      format.html {
+        render inline: "application_version: <%= @version[:application_version] %><br />
+                        assembly_version: <%= @version[:assembly_version] %>"
+      }
+    end
+  end
+
   private
 
   # Confirms a logged-in user.
